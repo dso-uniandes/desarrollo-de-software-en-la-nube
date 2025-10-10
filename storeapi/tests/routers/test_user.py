5292f88f15e20b8fa0e7bb55915fb511833c8d76
Fernando Parra <f.parrav@uniandes.edu.co>
2025-10-10 17:23:15 -0500

    Refactor login endpoint to use OAuth2PasswordRequestForm

--- storeapi/tests/routers/test_user.py
+++ storeapi/tests/routers/test_user.py
@@ -28,14 +28,17 @@ async def test_register_user_already_exists(async_client: AsyncClient, registere
 
 @pytest.mark.anyio
 async def test_login_user_not_exists(async_client: AsyncClient):
-    response = await async_client.post("/token", json={"email": "test@example.net", "password": "1234"})
+    response = await async_client.post(
+        "/token",
+        data={"username": "test@example.net", "password": "1234"}
+    )
     assert response.status_code == 401
 
 
 @pytest.mark.anyio
 async def test_login_user(async_client: AsyncClient, registered_user: dict):
     response = await async_client.post(
-        "/token", json={"email": registered_user["email"], "password": registered_user["password"], },
+        "/token",
+        data={"username": registered_user["email"], "password": registered_user["password"], },
     )
     assert response.status_code == 200
-
